78e22f17fd738eed6235368ac1cabb91dbe31460
Serhii Rubtsov <rubtsov1986@bigmir.net>
2023-08-30 21:26:04 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit1111222333444555 <code>src/App.tsx</code> and save to reload.
+          Edit11112223334445558888 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
